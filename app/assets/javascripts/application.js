9285fd516a11bcb627f8e45bb8dc481326a73750
w3villa-harsh-kumar <harsh.kumar@w3villa.com>
2023-05-11 12:04:27 +0530

    Adding Favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown(); 
+});
